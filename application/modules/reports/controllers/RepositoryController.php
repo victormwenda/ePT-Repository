be745e9e9e58cc23dee1996a628faddd521e77fb
Victor Mwenda <vmwenda.vm@gmail.com>
2016-12-09 18:52:28 +0300

    Working on reports -- doing a query and return the data as a json array

--- application/modules/reports/controllers/RepositoryController.php
+++ application/modules/reports/controllers/RepositoryController.php
@@ -8,9 +8,9 @@ class Reports_RepositoryController extends Zend_Controller_Action
         /* Initialize action controller here */
         $ajaxContext = $this->_helper->getHelper('AjaxContext');
         $ajaxContext->addActionContext('index', 'html')
-                    ->addActionContext('report', 'html')
-                    ->initContext();
-        $this->_helper->layout()->pageName = 'report'; 
+            ->addActionContext('report', 'html')
+            ->initContext();
+        $this->_helper->layout()->pageName = 'report';
     }
 
     public function indexAction()
@@ -18,9 +18,9 @@ class Reports_RepositoryController extends Zend_Controller_Action
         if ($this->getRequest()->isPost()) {
             $params = $this->_getAllParams();
             $reportService = new Application_Service_Reports();
-            $response=$reportService->getParticipantDetailedReport($params);
+            $response = $reportService->getParticipantDetailedReport($params);
             $this->view->response = $response;
-            $this->view->type= $params['reportType'];
+            $this->view->type = $params['reportType'];
         }
         $provider = new Application_Service_Providers();
         $this->view->providers = $provider->getProviders();
@@ -29,12 +29,33 @@ class Reports_RepositoryController extends Zend_Controller_Action
     public function reportAction()
     {
         if ($this->getRequest()->isPost()) {
-            $params = $this->_getAllParams();            
+            $params = $this->_getAllParams();
             $reportService = new Application_Service_Importcsv();
             $reportService->getAllProviderDetailedReport($params);
         }
     }
 
+    public function testAction()
+    {
+        if (!class_exists('database\core\mysql\DatabaseUtils')) {
+            require_once 'C:\xampp\htdocs\ePT-Repository\database\core-apis\DatabaseUtils.php';
+
+        }
+        if (!class_exists('database\crud\SystemAdmin')) {
+            require_once 'C:\xampp\htdocs\ePT-Repository\database\crud\SystemAdmin.php';
+
+        }
+        if (!class_exists('database\modules\SystemAdminInfo')) {
+            require_once 'C:\xampp\htdocs\ePT-Repository\database\modules\SystemAdminInfo.php';
+
+        }
+
+        $databaseUtils = new \database\core\mysql\DatabaseUtils();
+        $systemAdmin = new \database\crud\SystemAdmin($databaseUtils);
+        $systemAdminInfo = new \database\modules\SystemAdminInfo("", "");
+        echo $systemAdminInfo->export_query_html($systemAdmin->query_from_system_admin(array(), array()));
+        exit;
+    }
 
 }
 
